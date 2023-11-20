1706248a151a10e835acca6457905bff5e29385c
Freddie Sackur <github@dustyfox.uk>
2023-11-20 13:55:14 +0000

    New-BlogPost completes tags

--- tools/New-Blogpost.ps1
+++ tools/New-Blogpost.ps1
@@ -1,11 +1,28 @@
-param(
-    [Parameter(Mandatory=$true)]
-    [string]$Title
+param
+(
+    [Parameter(Mandatory)]
+    [string]$Title,
+
+    [ArgumentCompleter({
+        $TagLines = Get-ChildItem $PSScriptRoot/../_posts, $PSScriptRoot/../_unpublished_posts |
+            Get-Content -TotalCount 10 |
+            Select-String ^tags
+        $Tags = $TagLines -replace '^tags:\s*' |
+            ForEach-Object {ConvertFrom-Yaml $_} |
+            Write-Output |
+            Sort-Object -Unique
+        $Tags = $Tags -replace '(.*\s.*)', "'`$1'"
+        $Tags
+    })]
+    [string[]]$Tag = 'Uncategorized'
 )
 
 $DateString = (Get-Date).ToString('yyyy-MM-dd HH:mm')
 $Filename = "$($DateString -replace ' .*')-$($Title -replace ' ', '-').md"
-$Path = Join-Path (Resolve-Path $PSScriptRoot\..\_posts) $Filename
+$Path = Join-Path (Resolve-Path $PSScriptRoot/../_unpublished_posts) $Filename
+
+$Tags = $Tag -replace '(.*\s.*)', "'`$1'"
+$TagString = $Tags -join ', '
 
 $Header = @"
 ---
@@ -14,7 +31,7 @@ title: $Title
 date: $DateString
 author: freddiesackur
 comments: true
-tags: [Uncategorized]
+tags: [$TagString]
 ---
 "@
 
