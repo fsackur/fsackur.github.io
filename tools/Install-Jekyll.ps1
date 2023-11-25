d729ae4e71ea8e7f98d3671b6c587cff348e97ea
Freddie Sackur <github@dustyfox.uk>
2023-11-25 15:51:37 +0000

    Install ruby on linux

--- tools/Install-Jekyll.ps1
+++ tools/Install-Jekyll.ps1
@@ -1,20 +1,46 @@
-if ($IsLinux)
-{
-    # TODO
-}
-else
-{
-    if (-not (Get-Command ruby -ErrorAction Ignore))
-    {
-        winget install RubyInstallerTeam.Ruby.3.2
-    }
+$AppPath = $PSScriptRoot | Split-Path
+Push-Location $AppPath
 
-    if (-not (Get-Command bundle -ErrorAction Ignore))
+try
+{
+    if ($IsWindows -or $PSVersionTable.PSVersion.Major -le 5)
     {
-        gem install bundler
-    }
+        if (-not (Get-Command ruby -ErrorAction Ignore))
+        {
+            winget install RubyInstallerTeam.Ruby.3.2
+        }
 
-    ridk install 1,3
+        if (-not (Get-Command bundle -ErrorAction Ignore))
+        {
+            gem install bundler
+        }
+
+        ridk install 1,3
+    }
+    else
+    {
+        if (-not (Get-Command ruby -ErrorAction Ignore))
+        {
+            $PackageMgr = Get-Command dnf -ErrorAction Ignore
+            if (-not $PackageMgr)
+            {
+                $PackageMgr = Get-Command apt -ErrorAction Ignore
+            }
+            sudo $PackageMgr install ruby-devel
+        }
+
+        if (-not (Get-Command bundle -ErrorAction Ignore))
+        {
+            gem install bundler
+        }
+
+        bundle config set --local path ./gem
+    }
 
     bundle install
+
+}
+finally
+{
+    Pop-Location
 }
